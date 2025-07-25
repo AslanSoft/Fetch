a9d004ebad68b3a5a3084f029a9369727ba9f1c9
AslanSoft <beggyplayy@gmail.com>
2025-07-25 21:33:45 +0500

    first commit

---
+++ Fetch/main.js
@@ -0,0 +1,26 @@
+"use strict"
+
+let productList = document.querySelector(".product-list")
+fetch("https://fakestoreapi.com/products",{
+ method:"GET",
+ headers:{
+  "Content-type":"application/json"
+ },
+})
+.then((response)=>response.json())
+.then((data)=>{
+ data.length ?data.map((e, index)=>{
+  let product = document.createElement("div")
+  product.classList.add("product")
+
+  product.innerHTML=`
+   <img src="${e.image}" width="250px" height="250px"/>
+   <h2>${e.title.slice(0,20)} ...</h2>
+   <h1 class="prices">price:</h1>
+  `
+  productList.appendChild(product)
+ }):console.log('no data') 
+})
+
+
+
